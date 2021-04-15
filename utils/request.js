7553edcd1fa2ca6f05caf68a356ab042e27906f2
Fendy <zf@fendy5.cn>
2021-04-15 22:00:03 +0800

    build：添加环境

--- utils/request.js
+++ utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: '/dev-api', // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_URL, // url = base url + request url
   withCredentials: true, // send cookies when cross-domain requests
   timeout: 30000 // request timeout
 })
